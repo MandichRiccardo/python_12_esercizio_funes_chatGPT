2e8c6c1e301c96d144b5705ba3ec5064ba5ccbfd
MandichRiccardo <ricky.mandich@gmail.com>
2024-03-25 17:06:27 +0100

    aggiornamento 2024 03 25 17:06

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # funzione che prende come parametro una lista e ritorna la sua somma
 def somma_lista(lista):
     """
-
     :type lista: list
     """
     somma = 0
@@ -13,7 +12,6 @@ def somma_lista(lista):
 # funzione che conta quante volte una parola è uguale all'elemento di una lista
 def conteggio_parola(lista, stringa):
     """
-
     :type lista: list
     :type stringa: string
     """
@@ -24,9 +22,47 @@ def conteggio_parola(lista, stringa):
     return count
 
 
+def unisci_dizionari(dizionario1, dizionario2):
+    """
+    :type dizionario1: dict
+    :type dizionario2: dict
+    :return: nuovo_dizionario: dict
+    """
+    nuovo_dizionario = {}
+    for i in dizionario1:
+        nuovo_dizionario[i] = dizionario1[i]
+    for i in dizionario2:
+        nuovo_dizionario[i] = dizionario2[i]
+    return nuovo_dizionario
+
+
+def rimuovi_duplicati(old_list):
+    """
+    :type old_list: list
+    :return: new_lista: list
+    """
+    new_list = []
+    for i in old_list:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+
 # corpo del programma
-for i in range(1, 11, 1):
-    listaInt = list(range(1, i + 1, 1))
-    print("la somma degli elementi della lista " + str(listaInt) + " è " + str(somma_lista(listaInt)))
+listaInt = list(range(1, 10 + 1, 1))
+print("la somma degli elementi della lista " + str(listaInt) + " è " + str(somma_lista(listaInt)))
+
 listaString = ["mario", "luigi", "luca", "gianni", "giovanni", "mario", "luca", "gianni", "giovanni", ]
 print("nella lista " + str(listaString) + "la stringa \"mario\" è contenuta " + str(conteggio_parola(listaString, "mario")) + " volte")
+
+persona1 = {"nome": "Mario", "cognome": "Rossi", "età": 30}
+persona2 = {"nome": "luigi", "cognome": "bianchi", "età": 25}
+persone = unisci_dizionari(persona1, persona2)
+print(persona1)
+print(persona2)
+print(persone)
+
+lista = ["mario", "luigi", "mario", "luigi"]
+print(lista)
+lista = rimuovi_duplicati(lista)
+print(lista)
